7493d747753f9a5415466e1aefc99d052661c376
Jordan <Jordan@DESKTOP-16MHI1C>
2023-05-16 20:19:56 -0700

    Add project files.

---
+++ Hash/Hash.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+uint64_t spark_hash(std::string str) {
+    uint64_t hash = 0x84222325CBF29CE4;
+    std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+    std::replace(str.begin(), str.end(), '/', '\\');
+    size_t semicolonIndex = str.find(';');
+    if (semicolonIndex != std::string::npos) {
+        str = str.substr(0, semicolonIndex);
+    }
+    for (char chr : str) {
+        hash = static_cast<uint64_t>(chr) ^ ((hash << 40) + hash * 0x1B3) & 0xFFFFFFFFFFFFFFFF;
+
+    }
+    return hash;
+}
+
+int main() {
+    std::string input;
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, input);
+    uint64_t result = spark_hash(input);
+    std::cout << "Hash: " << std::hex << std::setw(16) << std::setfill('0') << result << std::endl;
+    return 0;
+}
